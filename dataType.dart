aaa9945c491dd11edaa03dac3c58819f1bb10c28
Namukuru Lauryn <namukurulauryn@gmail.com>
2024-03-07 11:17:45 +0300

    The data types assignment.

---
+++ dataType.dart
@@ -0,0 +1,18 @@
+// This program demonstrates the usage of various data types in Dart: int, double, String, List, and Map.
+
+void main() {
+  int age = 30;
+  print("My age is $age.");
+
+  double pi = 3.14159;
+  print("The value of pi is $pi.");
+
+  String name = "Alice";
+  print("My name is $name.");
+
+  List<String> fruits = ["apple", "banana", "orange"];
+  print("My favorite fruits are: $fruits.");
+
+  Map<String, int> person = {"age": 30, "height": 175};
+  print("Person's information: $person");
+}
